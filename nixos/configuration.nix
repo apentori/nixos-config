bb6c7e27af539334c1043eac1e66b62f7618c543
Alexis Pentori <alexis@status.im>
2024-01-10 14:06:20 +0100

    fix
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,7 +15,7 @@
   # bootloader
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.usOSProber= true;
+  boot.loader.grub.useOSProber = true;
   
   networking.hostName = "pandora";
   networking.networkmanager.enable =  true;
