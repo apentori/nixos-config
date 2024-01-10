ba665a2b14d496d45090baf51d855fb8cdbc3f9c
Alexis Pentori <alexis@status.im>
2024-01-10 13:53:29 +0100

    pandora: fix config
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -17,33 +17,18 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.usOSProber= true;
   
+  networking.hostName = "pandora";
+  networking.networkmanager.enable =  true;
   
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
   nix.nixPath = ["/etc/nix/path"];
   environment.etc =
     lib.mapAttrs'
@@ -60,41 +45,59 @@
     auto-optimise-store = true;
   };
 
-  # FIXME: Add the rest of your current configuration
-
-  # TODO: Set your hostname
-  networking.hostName = "pandora";
-  networking.networkmanager.enable =  true;
-
   time.timeZone = "Europe/Madrid";
+
   i18n.defaultLocale = "en_US.UTF-8";
-  
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_ES.UTF-8";
+    LC_IDENTIFICATION = "es_ES.UTF-8";
+    LC_MEASUREMENT = "es_ES.UTF-8";
+    LC_MONETARY = "es_ES.UTF-8";
+    LC_NAME = "es_ES.UTF-8";
+    LC_NUMERIC = "es_ES.UTF-8";
+    LC_PAPER = "es_ES.UTF-8";
+    LC_TELEPHONE = "es_ES.UTF-8";
+    LC_TIME = "es_ES.UTF-8";
+  };
+
+
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver = {
-      layout = "us"
-      xkbVariant = "";
-    };
+
+  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit =true;
-      pulse.enable = true;
-    };
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     irotnep = {
-      # It's just for VM deployment 
-      initialPassword = "testOfNixOs";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
       ];
@@ -107,17 +110,7 @@
     };
   };
   nixpkgs.config.allowUnfree = true;
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
-    };
-  };
+
   environment.systemPackages = with pkgs; [
     vim
     wget
