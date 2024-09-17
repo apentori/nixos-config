ac27e25a97fc1db3052ee69e307d70dd032201cf
Alexis Pentori <alexis@status.im>
2024-09-17 20:49:37 +0200

    geth: update
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- roles/go-ethereum.nix
+++ roles/go-ethereum.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ...}:
+{ pkgs, config, lib, ...}:
 let
   gethPort=30303;
 in {
@@ -18,6 +18,18 @@ in {
     # Firewall
     networking.firewall.allowedTCPPorts = [ gethPort ];
     networking.firewall.allowedUDPPorts = [ gethPort ];
+    # Users
+    users.users.geth = {
+      group = "geth";
+      home = "/geth";
+      uid = 6000;
+      description = "Go ethereum service user";
+      isSystemUser = true;
+    };
+  
+    users.groups.geth = {
+      gid = 6000;
+    };
 
     services.geth.holesky = {
       enable = true;
@@ -38,6 +50,13 @@ in {
     };
 
     systemd.services.geth-holesky = {
+      serviceConfig = {
+        stateDir = lib.mkForce "/geth/data";
+        dataDir = lib.mkForce "/geth/data/holesky";
+        DynamicUser = lib.mkForce false;
+        User = "geth";
+        Group = "geth"; 
+      };
     };
   };
 }
