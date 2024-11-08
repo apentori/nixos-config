f12fca2a139f0935e99848258b022a215a807330
Alexis Pentori <alexis@status.im>
2024-11-08 07:46:06 +0100

    nextcloud: adding nginx services
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- roles/nextcloud.nix
+++ roles/nextcloud.nix
@@ -1,15 +1,21 @@
 { pkgs, secret, ... }:
 {
   age.secrets."nextcloud-admin" = {
-    file = ../secrets/services/nextcloud/admin;
+    file = ../secrets/services/nextcloud/admin.age;
     path = "/etc/nextcloud-admin-pass";
+    owner = "nextcloud";
+    group = "nextcloud";
   };
 
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud28;
-    hostName = "localhost";
+    hostName = "nextcloud.irotnep.net";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
-    datadir = "/data/nextcloud";
   };
+  services.nginx.virtualHosts."nextcloud.irotnep.net" = {
+    forceSSL = true;
+    enableACME = true;
+  };
+
 }
