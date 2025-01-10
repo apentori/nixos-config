3592f36a6f990f53f6176710d1c31e3cfda2b3b1
Alexis Pentori <alexis@status.im>
2025-01-10 17:37:54 +0100

    ghostfolio: use docker compose for now
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- roles/ghostfolio.nix
+++ roles/ghostfolio.nix
@@ -1,132 +1,98 @@
 { pkgs, lib, config, ...}:
 let
-  reddis_pass = "12345pass67890word";
+  writeTextDir = {
+  name = "ghostfolio_compose";
+  destination = "/data/ghostfolio/docker-compose.yml";
+  text = ''
+    name: ghostfolio
+    services:
+      ghostfolio:
+        image: docker.io/ghostfolio/ghostfolio:latest
+        container_name: ghostfolio
+        restart: unless-stopped
+        init: true
+        cap_drop:
+          - ALL
+        security_opt:
+          - no-new-privileges:true
+        env_file:
+          - /data/ghostfolio/.env
+        ports:
+          - 3333:3333
+        depends_on:
+          postgres:
+            condition: service_healthy
+          redis:
+            condition: service_healthy
+        healthcheck:
+          test: ['CMD-SHELL', 'curl -f http://localhost:3333/api/v1/health']
+          interval: 10s
+          timeout: 5s
+          retries: 5
+    
+      postgres:
+        image: docker.io/library/postgres:15-alpine
+        container_name: gf-postgres
+        restart: unless-stopped
+        cap_drop:
+          - ALL
+        cap_add:
+          - CHOWN
+          - DAC_READ_SEARCH
+          - FOWNER
+          - SETGID
+          - SETUID
+        security_opt:
+          - no-new-privileges:true
+        env_file:
+          - /data/ghostfolio/.env
+        healthcheck:
+          test:
+            ['CMD-SHELL', 'pg_isready -d "$${POSTGRES_DB}" -U $${POSTGRES_USER}']
+          interval: 10s
+          timeout: 5s
+          retries: 5
+        volumes:
+          - /data/ghostfolio/data:/var/lib/postgresql/data
+    
+      redis:
+        image: docker.io/library/redis:alpine
+        container_name: gf-redis
+        restart: unless-stopped
+        user: '999:1000'
+        cap_drop:
+          - ALL
+        security_opt:
+          - no-new-privileges:true
+        env_file:
+          - /data/ghostfolio/.env
+        command:
+          - /bin/sh
+          - -c
+          - redis-server --requirepass "$${REDIS_PASSWORD:?REDIS_PASSWORD variable is not set}"
+        healthcheck:
+          test:
+            ['CMD-SHELL', 'redis-cli --pass "$${REDIS_PASSWORD}" ping | grep PONG']
+          interval: 10s
+          timeout: 5s
+          retries: 5
+  '';
+  };
+
+
 in {
-  # Application
-  virtualisation.oci-containers.containers."ghostfolio-app" = {
-    image = "ghostfolio/ghostfolio:latest";
-    hostname = "ghostfolio";
-    ports = [ "3333:3333"];
-    environment = {
-      ACCESS_TOKEN_SALT = "acsaceccaceveve";
-      COMPOSE_PROJECT_NAME = "ghostfolio-development";
-      DATABASE_URL = "postgresql://postgres:Th1s1sAPgslGhostfolioPwd@ghostfolio-postgres:5432/ghostfolio-db?connect_timeout=300&sslmode=prefer";
-      JWT_SECRET_KEY = "acasclacsj236615ewce";
-      POSTGRES_DB = "ghostfolio-db";
-      POSTGRES_PASSWORD = "Th1s1sAPgslGhostfolioPwd";
-      POSTGRES_USER = "postgres";
-      REDIS_HOST = "ghostfolio-redis";
-      REDIS_PASSWORD = "12345pass67890word";
-      REDIS_PORT = "6379";
-    };
-    dependsOn = [
-      "ghostfolio-postgres"
-      "ghostfolio-redis"
-    ];
-    #environmentFile = [ /etc/ghostfolio/.env ];
-    log-driver = "journald";
-    extraOptions = [
-      "--health-cmd=curl -f http://localhost:3333/api/v1/health"
-      "--health-interval=10s"
-      "--health-retries=5"
-      "--health-timeout=5s"
-      "--network-alias=ghostfolio"
-      "--network=ghostfolio_default"
-    ];
+    age.secrets."ghostfolio-admin" = {
+    file = ../secrets/services/ghostfolio/env.age;
+    path = "/data/ghostfolio/.env";
+    owner = "irotnep";
+    group = "docker";
   };
 
-  systemd.services."docker-ghostfolio-ghostfolio" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 90 "no";
-    };
-    after = [ "docker-network-ghostfolio_default.service" ];
-    requires = [ "docker-network-ghostfolio_default.service" ];
-    partOf = [ "docker-compose-ghostfolio-root.target" ];
-    wantedBy = [ "docker-compose-ghostfolio-root.target" ];
-  };
-  virtualisation.oci-containers.containers."ghostfolio-postgres" = {
-    image = "postgres:15";
-    environment = {
-      POSTGRES_DB = "ghostfolio-db";
-      POSTGRES_PASSWORD = "Th1s1sAPgslGhostfolioPwd";
-      POSTGRES_USER = "postgres";
-    };
-    ports = ["5432:5432"];
-    volumes = [
-      "/data/ghostfolio:/var/lib/postgresql/data:rw"
-    ];
-    log-driver = "journald";
-    extraOptions = [
-      "--health-cmd=pg_isready -d \${POSTGRES_DB} -U \${POSTGRES_USER}"
-      "--health-interval=10s"
-      "--health-retries=5"
-      "--health-timeout=5s"
-      "--network-alias=postgres"
-      "--network=ghostfolio_default"
-    ];
-  };
-  systemd.services."docker-ghostfolio-postgres" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 90 "no";
-    };
-    after = [ "docker-network-ghostfolio_default.service" ];
-    requires = [ "docker-network-ghostfolio_default.service" ];
-    partOf = [ "docker-compose-ghostfolio-root.target" ];
-    wantedBy = [ "docker-compose-ghostfolio-root.target" ];
-  };
-  virtualisation.oci-containers.containers."ghostfolio-redis" = {
-    image = "redis:alpine";
-    environment = {
-      REDIS_HOST = "ghostfolio-redist";
-      REDIS_PASSWORD = "12345pass67890word";
-      REDIS_PORT = "6379";
-    };
-    ports = ["6379:637"];
-    cmd = [ "redis-server --requirepass " reddis_pass ];
-    log-driver = "journald";
-    extraOptions = [
-      "--health-cmd=redis-cli --pass ping | grep PONG"
-      "--health-interval=10s"
-      "--health-retries=5"
-      "--health-timeout=5s"
-      "--network-alias=redis"
-      "--network=ghostfolio_default"
-    ];
-  };
-  systemd.services."docker-ghostfolio-redis" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 90 "no";
-    };
-    after = [ "docker-network-ghostfolio_default.service" ];
-    requires = [ "docker-network-ghostfolio_default.service" ];
-    partOf = [ "docker-compose-ghostfolio-root.target" ];
-    wantedBy = [ "docker-compose-ghostfolio-root.target" ];
-  };
+ systemd.tmpfiles.rules = [
+    "d /data/ghostfolio 0750 irotnep docker"
+    "d /data/ghostfolio/data 0750 irotnep docker"
+];
 
-  # Networks
-  systemd.services."docker-network-ghostfolio_default" = {
-    path = [ pkgs.docker ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStop = "docker network rm -f ghostfolio_default";
-    };
-    script = ''
-      docker network inspect ghostfolio_default || docker network create ghostfolio_default
-    '';
-    partOf = [ "docker-compose-ghostfolio-root.target" ];
-    wantedBy = [ "docker-compose-ghostfolio-root.target" ];
-  };
-
-  # Root service
-  # When started, this will automatically create all resources and start
-  # the containers. When stopped, this will teardown all resources.
-  systemd.targets."docker-compose-ghostfolio-root" = {
-    unitConfig = {
-      Description = "Root target generated by compose2nix.";
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
     services.nginx.virtualHosts."ghostfolio.irotnep.net" = {
     forceSSL = true;
     enableACME = true;
